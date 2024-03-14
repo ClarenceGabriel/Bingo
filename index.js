62064a45207b2187077acb468647404224fe9320
ClarenceGabriel <gabrielclarence9@gmail.com>
2024-03-14 14:28:01 +0800

    Bingo

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ function generateNumber(max) {
         console.log('The number is in I.');
     }
     else if (randomNumber >= 31 && randomNumber <= 45) {
-        console.log('The number is in I.');
-    } 
+        console.log('The number is in N.');
+    }
+    else if (randomNumber >= 46 && randomNumber <= 60) {
+        console.log('The number is in G.');
+    }
+    else if (randomNumber >= 61 && randomNumber <= 75) {
+        console.log('The number is in O.');
+    }
     
     else {
         console.log('The number is invalid');
